46683ecc2c4303f83fe44f7573fa6c4bbac0881f
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2016-11-12 20:00:39 +0100

    Replace range with rangeRandom (prepare next commit for fastTests)

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from test_utils import run, InfiniteLoopException
 
 maxValue = 15
 
+rangeRandom = range
 
 class TestCase(unittest.TestCase):
     def _testIn(self, code, *needed):
@@ -20,28 +21,28 @@ class TestCase(unittest.TestCase):
 
     # Atoms
     def test_constant(self):
-        for i in range(maxValue):
+        for i in rangeRandom(maxValue):
             self._testIn('''a = %d;''' % i, i)
 
     # +
     def test_add(self):
-        for i in range(maxValue):
-            for j in range(maxValue):
+        for i in rangeRandom(maxValue):
+            for j in rangeRandom(maxValue):
                 self._testIn('''a = %d + %d;''' % (i, j), i + j)
 
     # -
     def test_sub(self):
-        for i in range(maxValue):
-            for j in range(maxValue):
+        for i in rangeRandom(maxValue):
+            for j in rangeRandom(maxValue):
                 self._testIn('''a = %d - %d;''' % (i, j), i - j)
 
     # (unary -)
     def test_unaryminus(self):
-        for i in range(maxValue):
+        for i in rangeRandom(maxValue):
                 self._testIn('''a = - %d;''' % i, -i)
 
     def test_not_binary(self):
-        for i in range(maxValue):
+        for i in rangeRandom(maxValue):
             self._testIn('''a = !%d;''' % i, ~i)
 
     def test_complex_expr(self):
@@ -53,15 +54,15 @@ class TestCase(unittest.TestCase):
         self._testIn('''x = (-3 - 2) + 10;''', 5)
 
     def test_affectation(self):
-        for i in range(15):
+        for i in rangeRandom(15):
             self._testIn('''a = %d; b = a + 13;''' % i, 13 + i)
             self._testIn('''a = %d; b = a + 13; c = !b;
             d = c - 2;''' % i, ~(i + 13) - 2)
 
     # simple if
     def _test_simple_if(self, op, strop):
-        for i in range(0, 15):
-            for j in range(0, 15):
+        for i in rangeRandom(0, 15):
+            for j in rangeRandom(0, 15):
                 expectedValue = 5 + (10 if op(i, j) else 20)
                 self._testIn('''
                 b = 20;
@@ -92,8 +93,8 @@ class TestCase(unittest.TestCase):
 
     # if with else
     def _test_simple_if_else(self, op, strop):
-        for i in range(0, 15):
-            for j in range(0, 15):
+        for i in rangeRandom(0, 15):
+            for j in rangeRandom(0, 15):
                 expectedValue = 5 + (10 if op(i, j) else 20)
                 self._testIn('''
                 if(%d %s %d)
@@ -127,9 +128,9 @@ class TestCase(unittest.TestCase):
 
     # if chain without else
     def _test_simple_if_chain(self, op, strop):
-        for i in range(0, 8):
-            for j in range(4, 10):
-                for k in range(6, 12):
+        for i in rangeRandom(0, 8):
+            for j in rangeRandom(4, 10):
+                for k in rangeRandom(6, 12):
                     b = 5
                     if op(i, j):
                         b = 10
@@ -177,9 +178,9 @@ class TestCase(unittest.TestCase):
 
     # if chain with else
     def _test_simple_if_chain_else(self, op, strop):
-        for i in range(0, 8):
-            for j in range(4, 10):
-                for k in range(6, 12):
+        for i in rangeRandom(0, 8):
+            for j in rangeRandom(4, 10):
+                for k in rangeRandom(6, 12):
                     b = 5
                     if op(i, j):
                         b = 10
@@ -232,8 +233,8 @@ class TestCase(unittest.TestCase):
         self._test_simple_if_chain_else(lambda x, y: x != y, "!=")
 
     def _test_simple_while(self, op, strop):
-        for i in range(0, 15):
-            for j in range(0, 15):
+        for i in rangeRandom(0, 15):
+            for j in rangeRandom(0, 15):
                 infiniteLoop = op(i, j)
 
                 code = '''
@@ -268,33 +269,33 @@ class TestCase(unittest.TestCase):
         self._test_simple_while(lambda x, y: x != y, "!=")
 
     def test_and_binary(self):
-        for i in range(16):
-            for j in range(16):
+        for i in rangeRandom(16):
+            for j in rangeRandom(16):
                 self._testIn('''x = %d && %d;''' % (i, j), i & j)
 
     def test_or_binary(self):
-        for i in range(16):
-            for j in range(16):
+        for i in rangeRandom(16):
+            for j in rangeRandom(16):
                 self._testIn('''x = %d || %d;''' % (i, j), i | j)
 
     def test_bonus_mul(self):
-        for i in range(16):
-            for j in range(16):
+        for i in rangeRandom(16):
+            for j in rangeRandom(16):
                 self._testIn('''x = %d * %d;''' % (i, j), i * j)
 
     def test_bonus_div(self):
-        for i in range(16):
-            for j in range(1, 16):
+        for i in rangeRandom(16):
+            for j in rangeRandom(1, 16):
                 self._testIn('''x = %d / %d;''' % (i, j), i // j)
 
     def test_bonus_mod(self):
-        for i in range(16):
-            for j in range(1, 16):
+        for i in rangeRandom(16):
+            for j in rangeRandom(1, 16):
                 self._testIn('''x = %d %% %d;''' % (i, j), i % j)
 
     def test_bonus_pow(self):
-        for i in range(5):
-            for j in range(5):
+        for i in rangeRandom(5):
+            for j in rangeRandom(5):
                 self._testIn('''x = %d ^ %d;''' % (i, j), i ** j)
 
     def test_bonus_overflow(self):
